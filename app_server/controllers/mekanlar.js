aadb7322492ca7a7808eccc1391937dbbebb1cc5
Gulnaraliyeva <glnr10.aliyeva@gmail.com>
2022-12-04 18:18:26 +0400

    odev7 update

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,84 +1,101 @@
 var express = require('express');
 var router = express.Router();
-const anaSayfa= function(req, res, next){
-    res.render('anasayfa',
-    {
-    baslik: "Anasayfa",
-    "sayfaBaslik":{
-        "siteAd":"Mekanbul",
-        "slogan":"Civardaki Mekanları Keşfet!"
-    },
-    "mekanlar":[
-        {
-            "ad":"Starbucks",
-            "puan":"4",
-            "adres":"SDÜ AVM",
-            "imkanlar":["Kahve", "Pasta"],
-            "mesafe":"1km"
-        },
-        {
-            "ad":"Bilgisayar Müh",
-            "puan":"3",
-            "adres":"SDÜ Batı Kampüsü",
-            "imkanlar":["Mühendis Gençler","Sinan hoca"],
-            "mesafe":"1km"
-        }
-        
-    ]
+const axios=require("axios");
+var apiSecenekleri={
+  sunucu:"https://mekanbul.gulnaraliyeva.repl.co",
+  apiYolu:"/api/mekanlar/"
+}
+var mesafeyiFormatla=function(mesafe){
+var yeniMesafe,birim;
+if(mesafe>1){
+  yeniMesafe =parseFloat( mesafe.toFixed(1));
+  birim=" km";
+}else{
+  yeniMesafe=parseInt(mesafe*100,10);
+  birim=" m"
+}
+return yeniMesafe+birim;
+}
+var anaSayfaOlustur=function(res,mekanListesi){
+var mesaj;
+console.log(mekanListesi)
+if(!(mekanListesi instanceof Array )){
+  mesaj="API  HATASI:Bir şeyler ters gitti ";
+  mekanListesi=[];
+}else{
+if(!mekanListesi.length){
+  mesaj="civarda herhangi bir mekan yok";
+}
+}
+res.render("anasayfa",{
+"baslik":"Anasayfa",
+"sayfaBaslik":{
+  "siteAd":"Mekanbul",
+  "slogan":"Civardaki Mekanları Keşfet"
+},
+mekanlar:mekanListesi,
+mesaj:mesaj
+});
+}
+   const anaSayfa=function(req, res) {
+     axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+      params:{
+        enlem:req.query.enlem,
+        boylam:req.query.boylam
+      }
+     }).then(function(response){
+      var i,mekanlar;
+      mekanlar=response.data;
+      for(i=0;i<mekanlar.length;i++){
+        mekanlar[i].mesafe= mesafeyiFormatla(mekanlar[i].mesafe);
+
+      }
+      anaSayfaOlustur(res,mekanlar);
+
+     }).catch(function(hata){
+      anaSayfaOlustur(res,hata);
+     });  
+   }
+
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+    "enlem":mekanDetaylari.koordinat[0],
+    "boylam":mekanDetaylari.koordinat[1]
+  }
+  res.render('mekanbilgisi',
+  {
+    mekanBaslik: mekanDetaylari.ad,
+    mekanDetay:mekanDetaylari
+  });
+}
+var hataGoster = function(res,hata){
+  var mesaj;
+  if(hata.response.status==404){
+    mesaj="404, Sayfa Bulunamadı!";
+  }
+  else{
+    mesaj=hata.response.status+" hatası";
+  }
+  res.status(hata.response.status);
+  res.render('error',{
+    "mesaj":mesaj
+  });
+};
+
+
+const mekanBilgisi = function (req, res) {
+  axios
+    .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+    .then(function (response) {
+      detaySayfasiOlustur(res, response.data);
+    })
+    .catch(function (hata) {
+      hataGoster(res, hata);
     });
-}
-
-
-const mekanBilgisi=function(req, res){
-    res.render('mekanbilgisi',
-      {
-        "baslik":"Mekan Bilgisi",
-        "mekanBaslik":"Starbucks",
-        "mekanDetay": {
-          "ad": "Starbucks",
-          "adres": "SDÜ AVM",
-          "puan": "4",
-          "imkanlar": ["Kahve", "Pasta"],
-          "koordinatlar": {
-            "enlem": "37.7",
-            "boylam": "30.5"
-          },
-          "saatler": [
-            {
-              "gunler":"Pazartesi-Cuma",
-              "acilis": "9:00",
-              "kapanis": "23:00",
-              "kapali": false
-            },
-            {
-              "gunler":"Cumartesi-Pazar",
-              "acilis": "10:00",
-              "kapanis": "22:00",
-               "kapali": false
-            },
-         ],
-          "yorumlar": [
-            {
-              "yorumYapan": "Gulnar Aliyeva",
-              "yorumMetni": "Kahveler güzel",
-              "tarih": "20 Ekim 2022",
-              "puan": "4"
-            },
-            {
-              "yorumYapan": "Gulnar Aliyeva",
-              "puan": "3",
-              "tarih": "20 Ekim 2022",
-              "yorumMetni": "İyi"
-            }
-
-         ]
-
-       }
-      });
-}
-const yorumEkle= function(req, res){
-    res.render('yorumekle',{"title":"Yorum Sayfası"});
-}
+};
+const yorumEkle = (req, res, next) => {
+    res.render('yorumekle',{title :"Yorum ekle"});
+};
 
 module.exports={
     anaSayfa,
